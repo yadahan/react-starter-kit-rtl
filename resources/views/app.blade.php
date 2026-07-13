faff7b05dacd670308ebdbae393372bc3a78ca42
Yaakov Dahan <yakidehan@gmail.com>
2026-07-14 00:00:45 +0300

    Enhance html tag with direction attribute
    Add 'dir' attribute to html tag for RTL support.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ in_array(app()->getLocale(), ['ar', 'he']) ? 'rtl' : 'ltr' }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
