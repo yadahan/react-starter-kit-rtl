3f3977c1830d73681f4da7700966b9c60b331490
Yaakov Dahan <yakidehan@gmail.com>
2026-07-14 00:05:52 +0300

    Refactor layout classes for direction support

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -42,7 +42,7 @@ export default function Welcome() {
                 </header>
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
-                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
+                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] ltr:lg:rounded-tl-lg rtl:lg:rounded-tr-lg ltr:lg:rounded-br-none rtl:lg:rounded-bl-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
                             <h1 className="mb-1 font-medium">
                                 Let's get started
                             </h1>
@@ -52,7 +52,7 @@ export default function Welcome() {
                                 We suggest starting with the following.
                             </p>
                             <ul className="mb-4 flex flex-col lg:mb-6">
-                                <li className="relative flex items-center gap-4 py-2 before:absolute before:top-1/2 before:bottom-0 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
+                                <li className="relative flex items-center gap-4 py-2 before:absolute before:top-1/2 before:bottom-0 before:start-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
                                     <span className="relative bg-white py-1 dark:bg-[#161615]">
                                         <span className="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]">
                                             <span className="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
@@ -63,7 +63,7 @@ export default function Welcome() {
                                         <a
                                             href="https://laravel.com/docs"
                                             target="_blank"
-                                            className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
+                                            className="ms-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Documentation</span>
                                             <svg
@@ -72,7 +72,7 @@ export default function Welcome() {
                                                 viewBox="0 0 10 11"
                                                 fill="none"
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                className="h-2.5 w-2.5"
+                                                className="h-2.5 w-2.5 rtl:-scale-x-100"
                                             >
                                                 <path
                                                     d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
@@ -83,7 +83,7 @@ export default function Welcome() {
                                         </a>
                                     </span>
                                 </li>
-                                <li className="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
+                                <li className="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:start-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]">
                                     <span className="relative bg-white py-1 dark:bg-[#161615]">
                                         <span className="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]">
                                             <span className="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
@@ -94,7 +94,7 @@ export default function Welcome() {
                                         <a
                                             href="https://laracasts.com"
                                             target="_blank"
-                                            className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
+                                            className="ms-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Laracasts</span>
                                             <svg
@@ -103,7 +103,7 @@ export default function Welcome() {
                                                 viewBox="0 0 10 11"
                                                 fill="none"
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                className="h-2.5 w-2.5"
+                                                className="h-2.5 w-2.5 rtl:-scale-x-100"
                                             >
                                                 <path
                                                     d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
@@ -127,7 +127,7 @@ export default function Welcome() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="relative -mb-px aspect-[335/364] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
+                        <div className="relative -mb-px aspect-[335/364] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ms-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none ltr:lg:rounded-r-lg rtl:lg:rounded-l-lg dark:bg-[#1D0002]">
                             {/* Laravel Logo */}
                             <svg
                                 className="w-full max-w-none translate-y-0 text-[#F53003] opacity-100 transition-all duration-750 dark:text-[#F61500] starting:opacity-0 motion-safe:starting:translate-y-6"
@@ -167,7 +167,7 @@ export default function Welcome() {
 
                             {/* 13 */}
                             <svg
-                                className="relative -mt-[6.6rem] -ml-8 w-[438px] max-w-none [--stroke-color:#1B1B18] lg:ml-0 dark:[--stroke-color:#FF750F]"
+                                className="relative -mt-[6.6rem] -ms-8 w-[438px] max-w-none [--stroke-color:#1B1B18] lg:ms-0 dark:[--stroke-color:#FF750F]"
                                 viewBox="0 0 440 392"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -382,7 +382,7 @@ export default function Welcome() {
                                     />
                                 </g>
                             </svg>
-                            <div className="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-t-none lg:rounded-r-lg dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
+                            <div className="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-t-none ltr:lg:rounded-r-lg rtl:lg:rounded-l-lg dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                         </div>
                     </main>
                 </div>
