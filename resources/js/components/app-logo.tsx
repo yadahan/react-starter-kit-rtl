679e5ee9588310de7b51e385bd2cca88f446dfe8
Yaakov Dahan <yakidehan@gmail.com>
2026-07-19 23:31:10 +0300

    Update class names for RTL support in app-logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,7 +6,7 @@ export default function AppLogo() {
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
+            <div className="ms-1 grid flex-1 text-start text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
                     Laravel Starter Kit
                 </span>
